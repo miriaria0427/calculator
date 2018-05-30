f471ed31769f62784bba49bef200bed7e210ac67
miriaria0427 <mayuka@techacademy.jp>
2018-05-31 07:30:09 +0900

    課題提出用

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate{
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
     
     
     @IBOutlet weak var leftNum: UITextField!
@@ -25,11 +25,6 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         
         picker.delegate = self;
         picker.dataSource = self;
-        leftNum.delegate = self
-        rightNum.delegate = self
-        leftNum.tag = 7
-        rightNum.tag = 7
-        
         picker.showsSelectionIndicator = true;
         
         // ピッカーにはじめに表示する項目を指定
@@ -45,21 +40,22 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
-    }
-    
-    //文字列制限 //なんか8桁までしか入力できないから明日直す
-    //func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        //self.pass(st:leftNum.text!, tag: leftNum.tag, textField: leftNum)
-        //self.pass(st:rightNum.text!, tag: rightNum.tag, textField: rightNum)
-        //return true
-    //}
-    
-    //func pass(st:String,tag:Int,textField:UITextField) {
-        //if st.count > tag {
-            //textField.text = String((st.prefix(tag)))
-        //}
-    //}
-    
+        
+        //NSNotificationCenterへ登録
+        NotificationCenter.default.addObserver(
+              self,
+              selector: #selector(textFieldDidChangeL),
+              name: NSNotification.Name.UITextFieldTextDidChange,
+              object: leftNum)
+      
+       //NSNotificationCenterへ登録
+         NotificationCenter.default.addObserver(
+             self,
+             selector: #selector(textFieldDidChangeR),
+             name: NSNotification.Name.UITextFieldTextDidChange,
+             object: rightNum)
+        }
+
     //コンポーネントの個数を返すメソッド
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -77,9 +73,6 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     
     //演算子選択時
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //確認用
-        //print("列: \(row)")
-        //print("値: \(dataList[row])")
         //ラベル表示
         symbolLabel.text = dataList[row]
     }
@@ -88,18 +81,21 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     @IBAction func calcButton(_ sender: Any) {
         
         let sLabel = symbolLabel.text
-        var result :Float = 0
-
-        //Float変換(leftNumがnilの場合は空文字とし、Floatに変換できなかった場合は0扱いとする）
+        var result :Double = 0
+        
+        //Double変換(leftNumがnilの場合は空文字とし、Doubleに変換できなかった場合は0扱いとする）
         let lText : String = leftNum.text ?? ""
-        let lNum : Float = Float(lText) ?? 0
+        let lNum : Double = Double(lText) ?? 0
         let rText : String = rightNum.text ?? ""
-        let rNum : Float = Float(rText) ?? 0
+        let rNum : Double = Double(rText) ?? 0
         
         //計算をする
         switch sLabel {
         case "+":
             result = lNum + rNum
+            print(lNum)
+            print(rNum)
+            print(result)
         case "-":
             result = lNum - rNum
         case "×":
@@ -111,17 +107,18 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
             result = 0
         }
         
-        //計算結果を文字列に戻す。最後の文字と最後から二番目の文字が.ならトリム
-        var num = String(result)
-        print(num.count)
-        print("末尾から2文字:\(num.suffix(2))")
-        if(num.suffix(2) == ".0"){
-           num = num.replacingOccurrences(of:".0", with:"")
-            resultLabel.text = String(num)
-        }else{
-        //計算結果を表示させる。
-        resultLabel.text = String(result)
-        }
+        //計算結果を文字列に戻して最後の文字と最後から二番目の文字が.0ならトリムして結果表示
+          let num = String(result)
+          if(num.suffix(2) == ".0"){
+             resultLabel.text = String(num.replacingOccurrences(of:".0", with:""))
+         }else{
+            resultLabel.text = String(result)
+         }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     //クリアボタンが押されたら入力値と結果をクリアする
@@ -130,16 +127,30 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         rightNum.text = ""
         resultLabel.text = ""
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
     @objc func dismissKeyboard(){
         // キーボードを閉じる
         view.endEditing(true)
+    }
+    
+    //入力範囲を9桁までにする(左テキストボックス)
+    @objc private func textFieldDidChangeL(notification: NSNotification) {
+        let textFieldString = notification.object as! UITextField
+        if let text = textFieldString.text {
+            if text.count > 9 {
+                leftNum.text = String(text.prefix(9))
+            }
+        }
+    }
+    
+    //入力範囲を9桁までにする(右テキストボックス)
+    @objc private func textFieldDidChangeR(notification: NSNotification) {
+        let textFieldString = notification.object as! UITextField
+        if let text = textFieldString.text {
+            if text.count > 9 {
+                rightNum.text = String(text.prefix(9))
+            }
+        }
     }
 
 }
